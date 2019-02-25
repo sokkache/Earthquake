0f22c240cf47b4cecf557f9fcd463efc9a4fcc52
Cheyenne Sokkappa <sokkache@uw.edu>
2019-02-25 10:51:42 -0800

    Updates

---
+++ precache-manifest.e4ff8effdc276542b9faa8cf159f886c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3aa880a2bf97df3b59489d546df23c4a",
+    "url": "/Earthquake/static/media/ground_crack.3aa880a2.svg"
+  },
+  {
+    "revision": "9f34a0cd86175fb5c764",
+    "url": "/Earthquake/static/js/runtime~main.9f34a0cd.js"
+  },
+  {
+    "revision": "68c58def3125ef21ab9a",
+    "url": "/Earthquake/static/js/main.68c58def.chunk.js"
+  },
+  {
+    "revision": "1003c8c29873809b8e0a",
+    "url": "/Earthquake/static/js/2.1003c8c2.chunk.js"
+  },
+  {
+    "revision": "68c58def3125ef21ab9a",
+    "url": "/Earthquake/static/css/main.f87be35d.chunk.css"
+  },
+  {
+    "revision": "46ef8c3b9147d6a6a2edc392fcff0fdc",
+    "url": "/Earthquake/index.html"
+  }
+];
